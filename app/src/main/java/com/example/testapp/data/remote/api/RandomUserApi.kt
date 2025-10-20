b5547ffcdff52b733be7db0f2794e3de3ce4ce0a
serhii_senkin <senkin199@gmail.com>
2025-10-20 15:59:12 +0300

    Add endpoint for requests

--- app/src/main/java/com/example/testapp/data/remote/api/RandomUserApi.kt
+++ app/src/main/java/com/example/testapp/data/remote/api/RandomUserApi.kt
@@ -9,4 +9,8 @@ interface RandomUserApi {
     // Returns 20 results by default as per requirements
     @GET("api/")
     suspend fun getUsers(@Query("results") results: Int = 20): RandomUserResponse
+
+    companion object {
+        const val BASE_URL = "https://randomuser.me/"
+    }
 }
